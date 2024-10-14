cebfc567c2136aeea839ae3de017e679c16598c4
aha618054 <aha@miu.edu>
2024-10-14 17:47:01 -0500

    update workshop

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
+import {useEffect, useState} from "react";
+import {v4 as uuidv4} from 'uuid';
+
+import Header from "./Header";
+import Footer from "./Footer";
+import List from "./List";
+import Todo from "./types/Todo";
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default function App() {
+    const [todos, setTodos] = useState<Todo[]>([]);
 
-export default App;
+    useEffect(() => {
+        async function getTodos() {
+            const response = await fetch('http://localhost:3004/todos');
+            const data = await response.json();
+            setTodos(data);
+        }
+
+        getTodos();
+    }, []);
+
+    const addNewTodo = (name: string) => {
+        setTodos([{id: uuidv4(), name, done: false}, ...todos])
+    }
+
+    const updateTodo = (newTodo: Todo) => {
+        const oldTodo = todos.find(todo => todo.id === newTodo.id);
+        if (oldTodo) {
+            oldTodo.done = newTodo.done;
+            setTodos([...todos]);
+        }
+    }
+
+    const deleteTodo = (id: number | string) => {
+        const filteredTodos = todos.filter(todo => todo.id !== id);
+        setTodos([...filteredTodos]);
+    }
+
+    const updateTodosAll = (done: boolean) => {
+        const newTodos = todos.map(todo => {
+            todo.done = done;
+            return todo;
+        });
+        setTodos(newTodos);
+    }
+
+    const deleteFinishedTodo = () => {
+        setTodos(todos.filter(todo => !todo.done));
+    }
+
+    return (
+        <div className="todo-container">
+            <div className="todo-wrap">
+                <Header onAddNewTodo={addNewTodo}/>
+                <List todos={todos} onUpdateTodo={updateTodo} onDeleteTodo={deleteTodo}/>
+                <Footer todos={todos} onUpdateTodosAll={updateTodosAll} onDeleteFinishedTodo={deleteFinishedTodo}/>
+            </div>
+        </div>
+    );
+}
