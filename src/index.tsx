c8c7783b471210ba06ec73e5c71dda7cf94553a5
aha618054 <aha@miu.edu>
2024-10-15 14:13:29 -0500

    update workshop axios

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+import User from "./user";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-    <App />
+    // <App />
+    <User />
 );
 
 // If you want to start measuring performance in your app, pass a function
